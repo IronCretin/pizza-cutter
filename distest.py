694f59edf90950d2d9143790faac62bd6eb9dadf
Paul Maynard <palu.maynard@gmail.com>
2018-10-19 09:56:59 -0400

    Sppoky scary skeleton code

---
+++ distest.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import genline as gl
+import sys
+
+
+def main():
+    amt = int(sys.argv[1])
+    # point test
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ genline.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def gen_point():
+    ...
+
+def two_points():
+    ...
+
+def point_angle():
+    ...
+
+def point_chord():
+    ...
+
+def edge_angle():
+    ...
