2f30683f104eef5b3fb6edccc8861d56d20bf7fb
Paul Maynard <palu.maynard@gmail.com>
2018-10-19 19:50:41 -0400

    Remove numpy and added type hints

--- pttest.py
+++ pttest.py
@@ -2,10 +2,24 @@
 Test renderer for point generation
 """
 
+from typing import Sequence, Iterable, TypeVar
 import matplotlib.pyplot as plt
-import numpy as np
-from genline import gen_point
+from genline import gen_point, edge_points
 import sys
+
+
+T = TypeVar('T')
+
+
+def transpose(arr: Iterable[Sequence[T]]) -> Sequence[Sequence[T]]:
+    """
+    Transposes 2d sequence. Useful for getting lists of x and y coordinate from
+    a list of points.
+
+    >>> transpose([(1, 2), (3, 4), (5, 6)])
+    ((1, 3, 5), (2, 4, 6))
+    """
+    return tuple(zip(*arr))
 
 
 def main():
@@ -18,8 +32,8 @@ def main():
     plt.gca().set_aspect('equal', adjustable='box')
 
     ax.add_artist(plt.Circle((0, 0), 1, fill=False))
-    pts = np.vstack([gen_point() for _ in range(amt)]).T
-    plt.plot(pts[0], pts[1], '.', markersize=1, alpha=.5)
+    pts = transpose(gen_point() for _ in range(amt))
+    plt.plot(pts[0], pts[1], '.', markersize=1)
 
     plt.show()
 
